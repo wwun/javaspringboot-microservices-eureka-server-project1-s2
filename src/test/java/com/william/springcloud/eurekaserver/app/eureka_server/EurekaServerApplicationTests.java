b80317a9409180a580076afb42e65cb23d096c62
William Wun <williamwun@hotmail.com>
2024-09-20 10:33:41 -0700

    s3 finished eureka server load balance

---
+++ src/test/java/com/william/springcloud/eurekaserver/app/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.springcloud.eurekaserver.app.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
